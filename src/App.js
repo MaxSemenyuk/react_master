041b6ffd5510255776bd0c0c738ab8fdc833f814
MaxSemenyuk <maxsemen@gmail.com>
2022-06-21 14:49:14 +0300

    simpsons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import Char from "./components/Char";
 function App() {
   return (
     <div>
-        <Char name = 'Homer Jay Simpson' desc = '(born May 12, 1956)[36] is the main protagonist of The Simpsons series (or show). He is the spouse of Marge Simpson and father of Bart, Lisa and Maggie Simpson. Homer is overweight (said to be ~240 pounds), lazy, and often ignorant to the world around him.' pic = 'https://static.wikia.nocookie.net/simpsons/images/b/bd/Homer_Simpson.pngn'/>;
-        <Char name = '' desc = '' pic = ''/>
-        <Char name = '' desc = '' pic = ''/>
-        <Char name = '' desc = '' pic = ''/>
+        <Char name = 'Homer Jay Simpson'
+              desc = '(born May 12, 1956)[36] is the main protagonist of The Simpsons series (or show). He is the spouse of Marge Simpson and father of Bart, Lisa and Maggie Simpson. Homer is overweight (said to be ~240 pounds), lazy, and often ignorant to the world around him.'
+              pic = 'https://static.wikia.nocookie.net/simpsons/images/b/bd/Homer_Simpson.png'/>;
 
     </div>
-  );
+  )
 }
 export default App;
--- src/components/Char.js
+++ src/components/Char.js
@@ -1,9 +1,10 @@
 function Char(props){
     const {name, desc, pic} = props;
+    return(
     <div>
-        <h2>{name}</h2>;
-        <p>{desc}</p>;
-        <img src={pic}/>;
-    </div>
+        <h2>{name}</h2>
+        <p>{desc}</p>
+        <img src={pic}/>
+    </div>)
 }
 export default Char;
